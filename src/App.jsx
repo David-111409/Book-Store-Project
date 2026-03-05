809c714679230b774e3dfad212b21aaeb84a14f5
David-111409 <muhammaddawoud610@gmail.com>
2026-03-06 00:49:46 +0200

    about page end

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import Header from "./components/header/Header";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import Header from "./components/header/Header";
+import Slider from "./components/slider/Slider";
+import Services from "./components/services/Services";
+import { Route, Routes } from "react-router-dom";
+import About from "./pages/about/About";
 function App() {
   return (
     <>
       <Header />
+      {/* <Slider />
+      <Services /> */}
+      <Routes>
+        <Route path="/about" element=<About /> />
+      </Routes>
     </>
   );
 }
