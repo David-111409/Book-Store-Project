24e08930e88f74b67497b8761354395768549dde
David-111409 <muhammaddawoud610@gmail.com>
2026-03-06 05:54:41 +0200

    handling scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Header from "./components/header/Header";
 import About from "./pages/about/About";
 import Login from "./pages/forms/Login";
@@ -9,8 +9,14 @@ import Register from "./pages/forms/Register";
 import Home from "./pages/home/Home";
 import Footer from "./components/footer/Footer";
 import Contact from "./pages/contact/contact";
+import { useEffect } from "react";
 
 function App() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  
   return (
     <>
       <ToastContainer position="top-center" autoClose={1800} hideProgressBar={true} />
